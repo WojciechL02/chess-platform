cad9171470f25df33c39b52b12fe8461494c7b80
WojciechL02 <wojciech.lapacz02@gmail.com>
2026-04-13 21:31:08 +0200

    Mongo added

--- backend/services/game-service/app/storage_utils.py
+++ backend/services/game-service/app/storage_utils.py
@@ -1,10 +1,13 @@
 import redis.asyncio as redis
+from pymongo import AsyncMongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 
 
 load_dotenv()
 REDIS_URL = os.environ.get("REDIS_URL")
+MONGO_URL = os.getenv("MONGO_URL")
 
 
 async def init_redis(app):
@@ -13,3 +16,11 @@ async def init_redis(app):
 
 async def close_redis(app):
     await app.state.redis.close()
+
+
+async def init_mongo(app):
+    app.state.mongo = AsyncMongoClient(MONGO_URL, server_api=ServerApi('1'))
+
+
+async def close_mongo(app):
+    await app.state.mongo.close()
